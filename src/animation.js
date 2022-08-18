6548175d701820a88e299062a18ecb14654b5dd4
Aventia <lauralis0405@gmail.com>
2022-08-18 22:15:41 +0200

    added js and some css

--- src/animation.js
+++ src/animation.js
@@ -1,5 +1,21 @@
-function ratingClick() {
-    var chosenNum = document.getElementsByClassName('ratings-element');
+var container = document.getElementById("ratings"); // get container for the buttons
+var buttons = container.getElementsByClassName("ratings-element"); //using the container get all of its buttons
 
-    alert(chosenNum);
+for (var i = 0; i < buttons.length; i++) { 
+    buttons[i].addEventListener('click', function() { //add event listener when user clicks on btns
+        var current = document.getElementsByClassName("active");
+        var chosenNum = current.textContent;
+
+        if (current.length > 0) { //gets rid of clicked divs 
+            current[0].className = current[0].className.replace(" active", "");
+        }
+
+        this.className += " active";
+        document.getElementById("chosenNumber").innerHTML = chosenNum;
+    });
+}
+
+function submitRating() {
+    document.getElementById("hidden-container").style.display = "block";
+    document.getElementById("container").style.display = "none";
 }
