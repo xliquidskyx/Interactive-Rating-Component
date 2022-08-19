864c7aa212b1b31f5db89fac3c9c6b9c739b2d14
Aventia <lauralis0405@gmail.com>
2022-08-19 18:49:10 +0200

    added css and fixed some js bugs

--- src/animation.js
+++ src/animation.js
@@ -1,21 +1,24 @@
 var container = document.getElementById("ratings"); // get container for the buttons
 var buttons = container.getElementsByClassName("ratings-element"); //using the container get all of its buttons
-
+var current;
+var chosenNum;
 for (var i = 0; i < buttons.length; i++) { 
     buttons[i].addEventListener('click', function() { //add event listener when user clicks on btns
-        var current = document.getElementsByClassName("active");
-        var chosenNum = current.textContent;
-
+        current = document.getElementsByClassName("active");
         if (current.length > 0) { //gets rid of clicked divs 
-            current[0].className = current[0].className.replace(" active", "");
+            current[0].className = current[0].className.replace("active", "ratings-element");
         }
-
-        this.className += " active";
-        document.getElementById("chosenNumber").innerHTML = chosenNum;
+        this.className = "active";
     });
 }
 
 function submitRating() {
-    document.getElementById("hidden-container").style.display = "block";
-    document.getElementById("container").style.display = "none";
+    if (current == undefined) {
+        alert("Choose one of the options above"); //TODO: customize alert box
+    } else {
+        document.getElementById("hidden-container").style.display = "flex";
+        document.getElementById("container").style.display = "none";
+        var chosenNumber = document.querySelector(".active");
+        document.querySelector(".info").innerHTML = "You selected " + chosenNumber.textContent + " out of 5";
+    }
 }
